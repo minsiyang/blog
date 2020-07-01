bf6c1a81796354e10c994f9d72dbafa073f39c6a
minsi yang <yangminsi@gmail.com>
2020-07-01 18:01:36 +0100

    change root to welcome/index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  root 'welcome#index'
+  # map the root of application to 'welcome/index' for localhost:3000
 end
