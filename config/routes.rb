c076f6b49e5be34de1c0b0a5cc80d932a261620d
minsi yang <yangminsi@gmail.com>
2020-07-01 20:55:01 +0100

    Added article routes with resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :articles
+  # resources method can be use to declare a standard REST resource.
+
   root 'welcome#index'
   # map the root of application to 'welcome/index' for localhost:3000
 end
