763f0f9aba316a6ff5ee28f0d269a0a8c53eb6cc
minsi yang <yangminsi@gmail.com>
2020-07-01 22:01:24 +0100

    saving input article title and text into database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    # saving the params hash from the form input into article model as an instance
+    @article = Article.new(article_params)
+    #  saving the model in the database
+    @article.save
+    redirect_to @article
+  end
+  
+  private
+#   avoid wrongful mass assignment, only require and allow the one that we need
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
