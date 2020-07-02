eb48f8018603eb9777f2eb2154cf304ff03f782a
minsi yang <yangminsi@gmail.com>
2020-07-02 21:31:41 +0100

    Added name column to user table, added registeration controller and override sign_up_params and account_update_params method

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_154621) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
