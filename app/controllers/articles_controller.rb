50530987dc2a9dbd8c2514e4c344f837423eae14
minsi yang <yangminsi@gmail.com>
2020-07-02 23:07:39 +0100

    Added validation for article database title column, edit create route, and added new route for fail .save method to database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,23 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
-  def create
-    # saving the params hash from the form input into article model as an instance
-    @article = Article.new(article_params)
-    #  saving the model in the database
-    @article.save
-    redirect_to @article
-  end
+  
+    def new
+      @article = Article.new
+    end
+    
+    def create
+      # saving the params hash from the form input into article model as an instance
+      @article = Article.new(article_params)
+      #  saving the model in the database
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
+    end
+
+  
   
   private
 #   avoid wrongful mass assignment, only require and allow the one that we need
