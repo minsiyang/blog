753e7458754759a9fc9e67869a78119d1925f8c4
minsi yang <yangminsi@gmail.com>
2020-07-04 14:53:41 +0100

    Added notes for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    # find the aritcle id from comment db
     @article = Article.find(params[:article_id])
+    # create an comment for that article and insert in comment db
     @comment = @article.comments.create(comment_params)
+    # redirect back to current article path for show action
     redirect_to article_path(@article)
   end
 
   def destroy
+    # find the aritcle from id from comment db
     @article = Article.find(params[:article_id])
+    # find the related comment with it's own id
     @comment = @article.comments.find(params[:id])
+    # delete that comment
     @comment.destroy
+    # redirect back to current article path in the show action
     redirect_to article_path(@article)
   end
 
