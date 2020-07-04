93e856d0ffc8868807d4c6daa800a6a48082ed44
minsi yang <yangminsi@gmail.com>
2020-07-04 11:45:29 +0100

    Added relationship between comment and article models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+# Each article can have many comments
+    has_many :comments  
     validates :title, presence: true,
                       length: { minimum: 5 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+# Each comment belongs to one article
   belongs_to :article
 end
