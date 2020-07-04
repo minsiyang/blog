e0f3890c40c2d221457619912435163b8a852b1c
minsi yang <yangminsi@gmail.com>
2020-07-04 14:58:50 +0100

    Added 'dependent option in article model for comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,18 +4,18 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     # create an comment for that article and insert in comment db
     @comment = @article.comments.create(comment_params)
-    # redirect back to current article path for show action
+    # redirect back to current article path for show action for the article
     redirect_to article_path(@article)
   end
 
   def destroy
     # find the aritcle from id from comment db
     @article = Article.find(params[:article_id])
-    # find the related comment with it's own id
+    # locate the comment within the @article.comments array.
     @comment = @article.comments.find(params[:id])
     # delete that comment
     @comment.destroy
-    # redirect back to current article path in the show action
+    # redirect back to current article path in the show action for the article
     redirect_to article_path(@article)
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 # Each article can have many comments
-    has_many :comments  
+    has_many :comments, dependent: :destroy  
     validates :title, presence: true,
                       length: { minimum: 5 }
 end
